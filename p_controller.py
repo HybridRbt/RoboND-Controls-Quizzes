260a4ebd0c78febead4e50eb5ea529e216211977
Jianfeng Yang <jianfeng.yang0120@gmail.com>
2017-09-22 16:47:38 -0500

    add integral part to the controller

--- p_controller.py
+++ p_controller.py
@@ -1,80 +1,81 @@
 ##################################################################################
-# Your goal is to follow the comments and complete the tasks asked of you.
-# Take this time to try and understand the workings of the empty class structure.
-# The following quizzes will assume a understanding of a working class strucutre.
+# Your goal is to follow the comments and complete the the tasks asked of you.
 #
-# Good luck designing your proportional controller!
+# Good luck designing your proportional integral controller!
 #
 ##################################################################################
 
-class P_Controller:
-    def __init__(self, kp = 0.0, start_time = 0):
 
-        # The P controller can be initalized with a specific kp value
+class PI_Controller:
+    def __init__(self, kp = 0.0, ki = 0.0, start_time = 0):
+
+        # The PI controller can be initalized with a specific kp value
+        # and ki value
         self.kp_ = float(kp)
+        self.ki_ = float(ki)
 
-        # Create internal class variables for
-        # set_point_ and set it to 0.0, and start_time_
-        # and set it to the start_time variable.
+        # Define error_sum_ and set to 0.0
         ########################################
+        self.error_sum_ = 0.0
+        ########################################
+
+        # Store relevant data
+        self.last_timestamp_ = 0.0
         self.set_point_ = 0.0
         self.start_time_ = start_time
-        ########################################
-
-        # Store last timestamp
-        self.last_timestamp_ = 0.0
 
         # Control effort history
         self.u_p = [0]
+        self.u_i = [0]
 
-    # Set the altitude set point
     def setTarget(self, target):
         self.set_point_ = float(target)
 
     def setKP(self, kp):
-        # Set the internal kp_ value with the provided variable
-        # See setTarget if you are confused on how to do so
+        self.kp_ = float(kp)
+
+    def setKI(self, ki):
+        # Set the internal ki_ value with the provided variable
         ########################################
-        self.kp_ = kp
+        self.ki_ = float(ki)
         ########################################
 
     def update(self, measured_value, timestamp):
-        # Calculate delta_time using the last_timestamp_
-        # and the provided timestamp argument
-        ########################################
         delta_time = timestamp - self.last_timestamp_
-        ########################################
-
         if delta_time == 0:
             # Delta time is zero
             return 0
 
-        # Calculate the error as the differnce between
-        # the set_point_ and the measured_value
-        ########################################
+        # Calculate the error
         error = self.set_point_ - measured_value
-        ########################################
 
-        # Set the last_timestamp_ to current timestamp
-        ########################################
+        # Set the last_timestamp_
         self.last_timestamp_ = timestamp
+
+        # Calculate the error_sum_
+        ########################################
+        self.error_sum_ += error * delta_time
         ########################################
 
-        # Calculate the proportional error here. Be sure to access the
-        # the internal Kp class variable
-        ########################################
+        # Proportional error
         p = self.kp_ * error
+
+        # Calculate the integral error here. Be sure to access the
+        # the internal Ki class variable
+        ########################################
+        i = self.ki_ * self.error_sum_
         ########################################
 
         # Set the control effort
         # u is the sum of all your errors. In this case it is just
-        # the proportional error.
+        # the proportional and integral error.
         ########################################
-        u = p
+        u = p + i
         ########################################
 
         # Here we are storing the control effort history for post control
         # observations.
         self.u_p.append(p)
+        self.u_i.append(i)
 
         return u
